56a72b1165843d2b38d882150bd64d2c8d4c0597
David <david@retailcommon.com>
2015-02-01 19:07:34 -0500

    Created project

---
+++ app/src/main/java/com/dmchoull/daybreak/NewAlarmActivity.java
@@ -0,0 +1,66 @@
+package com.dmchoull.daybreak;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+
+public class NewAlarmActivity extends ActionBarActivity {
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void newAlarm(View view) {
+        TimePicker time = (TimePicker) findViewById(R.id.timePicker);
+        Integer hour = time.getCurrentHour();
+        Integer minute = time.getCurrentMinute();
+
+        alarmManager = (AlarmManager) getBaseContext().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(getBaseContext(), 0, intent, 0);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
+    }
+}
