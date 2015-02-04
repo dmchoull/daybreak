61a969e3ceab1836d9925078b9fd982eb233e032
David <david@retailcommon.com>
2015-02-03 23:07:44 -0500

    Set up Robolectric and android-unit-test plugin

---
+++ app/src/test/java/com/dmchoull/daybreak/NewAlarmActivityTest.java
@@ -0,0 +1,14 @@
+package com.dmchoull.daybreak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class NewAlarmActivityTest {
+
+    @Test
+    public void testSomething() throws Exception {
+
+    }
+}
