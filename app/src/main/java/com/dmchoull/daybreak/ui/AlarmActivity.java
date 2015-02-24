9a78e6374ae325d52a8534820daeebf81d8ab457
David <david@retailcommon.com>
2015-02-23 23:05:53 -0500

    Added placeholder alarm sound

--- app/src/main/java/com/dmchoull/daybreak/ui/AlarmActivity.java
+++ app/src/main/java/com/dmchoull/daybreak/ui/AlarmActivity.java
@@ -1,12 +1,20 @@
 package com.dmchoull.daybreak.ui;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.dmchoull.daybreak.R;
 
+import java.io.IOException;
+
 public class AlarmActivity extends DaybreakBaseActivity {
+    private MediaPlayer mediaPlayer;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm);
@@ -19,6 +27,28 @@ public class AlarmActivity extends DaybreakBaseActivity {
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        playAlarm();
+    }
+
+    private void playAlarm() {
+        mediaPlayer = new MediaPlayer();
+        Uri toneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        try {
+            mediaPlayer.setDataSource(this, toneUri);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override protected void onDestroy() {
+        mediaPlayer.stop();
+        super.onDestroy();
     }
 
     public void dismissAlarm(View view) {
