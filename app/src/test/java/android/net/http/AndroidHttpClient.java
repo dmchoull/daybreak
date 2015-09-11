dde3d96a28a80c98a8726814827a00542b8c30e7
David <david@retailcommon.com>
2015-09-11 10:22:40 -0400

    Updated build tools to 23.01

---
+++ app/src/test/java/android/net/http/AndroidHttpClient.java
@@ -0,0 +1,9 @@
+package android.net.http;
+
+/**
+ * https://github.com/robolectric/robolectric/issues/1862
+ *
+ * https://developer.android.com/preview/behavior-changes.html#behavior-apache-http-client
+ */
+public class AndroidHttpClient {
+}
